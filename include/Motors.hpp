37cd767ba25c34dd4c118f97e5457c9ac104d16b
Mactar1233 <Mactarg1233@gmail.com>
2021-04-09 10:25:33 -0700

    commit on main

---
+++ include/Motors.hpp
@@ -0,0 +1,8 @@
+#include "main.h";
+
+// declaring motors
+extern pros::Motor leftback;
+extern pros::Motor leftfront;
+extern pros::Motor rightback; 
+extern pros::Motor rightfront; 
+extern pros::Controller badstick; 
