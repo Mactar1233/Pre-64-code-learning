a6056d7e1f9f977ba859ec83f8809e3c0be1c847
Mactar1233 <Mactarg1233@gmail.com>
2021-04-25 17:38:06 -0700

    changed ports

--- src/Motors.cpp
+++ src/Motors.cpp
@@ -1,7 +1,7 @@
 #include "main.h";
 
-pros::Motor leftback(11,pros::E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_COUNTS);
-pros::Motor leftfront(2,pros::E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_COUNTS);
-pros::Motor rightback(20,pros::E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_COUNTS);
-pros::Motor rightfront(1,pros::E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_COUNTS);
+pros::Motor leftback(11,pros::E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_COUNTS);
+pros::Motor leftfront(10,pros::E_MOTOR_GEARSET_18, false, pros:: E_MOTOR_ENCODER_COUNTS);
+pros::Motor rightback(20,pros::E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_COUNTS);
+pros::Motor rightfront(1,pros::E_MOTOR_GEARSET_18, true, pros:: E_MOTOR_ENCODER_COUNTS);
 pros::Controller badstick(pros::E_CONTROLLER_MASTER);
